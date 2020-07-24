9d0c8647aca23fffa265f823669cf2a412d1bf79
양시영 <yangsiyeong@yangsiyeong-ui-MacBookPro.local>
2020-07-24 20:30:22 +0900

    Project Setting

---
+++ src/main/java/com/mashup/poten/PotenApplication.java
@@ -0,0 +1,13 @@
+package com.mashup.poten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mashup/poten/PotenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashup.poten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
