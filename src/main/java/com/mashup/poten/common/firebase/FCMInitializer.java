974bb3a6878a954bebce5707cff3263602261583
Seunghyeon-Kim <ssseungzz7@gmail.com>
2020-09-29 01:13:44 +0900

    [User] 김승현 [Details] FCM 설정 FCM 초기화 클래스 작성

---
+++ src/main/java/com/mashup/poten/common/firebase/FCMInitializer.java
@@ -0,0 +1,31 @@
+package com.mashup.poten.common.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+
+@Service
+public class FCMInitializer {
+
+    private static final String FIREBASE_CONFIG_PATH = "poten-backend-firebase-adminsdk-s1f18-3b678fa286.json";
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(FIREBASE_CONFIG_PATH).getInputStream()))
+                    .build();
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
